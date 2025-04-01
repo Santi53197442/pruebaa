551d554f190ba96c8ebf2311c7a79d885d9f7743
Diego Pereyra <diegopirazabal@gmail.com>
2025-04-01 12:54:20 -0300

    primer clase

---
+++ backend/src/main/java/com/capibus/buses/bus/Bus.java
@@ -0,0 +1,20 @@
+package com.capibus.buses.bus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="vehiculos")
+public class Bus {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
