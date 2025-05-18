66acf5e98df24dbed4ac13f3b954452b45d913c8
Santi53197442 <santipereyra64@gmail.com>
2025-05-18 19:22:41 -0300

    2

--- backend/src/main/java/com/omnibus/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/omnibus/backend/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.omnibus.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -40,5 +42,9 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); // Aplica a todos los endpoints
         return source;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
