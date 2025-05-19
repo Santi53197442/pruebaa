a4265175bc17270856c0d592059cb4989f9ee208
Santi53197442 <santipereyra64@gmail.com>
2025-05-19 12:39:44 -0300

    OE

--- backend/src/main/java/com/omnibus/backend/BusesApplication.java
+++ backend/src/main/java/com/omnibus/backend/BusesApplication.java
@@ -1,23 +1,30 @@
 package com.omnibus.backend;
 
-import com.omnibus.backend.config.DatabaseConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BusesApplication implements CommandLineRunner {
-
-	@Autowired
-	private DatabaseConfig databaseConfig;
+public class BusesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusesApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		databaseConfig.validarConexion();
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 }
+
